ef25804ec74dedc6fadd802876b28b63d24251de
Ammarah Ahmed <ammarah.rafia.ahmed@emory.edu>
2023-04-02 23:30:53 -0400

    attempt 1 continued

--- analysis/ahmed-a-hw4-analysis.R
+++ analysis/ahmed-a-hw4-analysis.R
@@ -4,10 +4,10 @@ pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table
 final.data <- read_rds("data/output/final_data.rds")
 
 #1
-final.data2 <- final.data %>% filter(snp == 'No' & partd == 'No' & !(planid %in% 800:899)) 
+final.data2 <- final.data %>% filter(snp == 'No' & !is.na(partc_score) & !(planid %in% 800:899)) 
 plan_count<- final.data2 %>% group_by(fips, year, plan_type) %>% count() %>% arrange(-n)
 
-plan.count.plot <- 
+plan.count.plot <- ggplot(final.data2, aes(x = ))
 
 plan_count %>% group_by(fips, year) %>% 
   select(fips, year) %>% 
@@ -15,16 +15,6 @@ plan_count %>% group_by(fips, year) %>%
   summarise(count = count()) %>%
   ggplot(aes(x = count, y = year)) +
   geom_boxplot(fill="blue", colour = "black", alpha=0.9) +
-  labs(title = "Distribution of Plan Counts by County", x = "Year", y = "Plan Counts") +
-  theme_bw()
-
-plan.count.plot <- plan_count %>% 
-  group_by(fips, year) %>%
-  select(fips, year, plan_type) %>% 
-  group_by(fips, year, plan_type) %>%
-  summarise(plan_co= count()) %>%
-  ggplot(aes(x = year, y = plan_co)) +
-  geom_boxplot(fill="blue", colour = "black", alpha = 0.9) +
   labs(title = "Distribution of Plan Counts by County", x = "Year", y = "Plan Counts") +
   theme_bw()
 
@@ -55,7 +45,7 @@ ggplot(aes(x = as.factor(Star_Rating))) +
   scale_fill_grey() + 
   labs(title = "Distribution of Star Ratings", x = "Star Ratings", y = "Count of Plans", fill="Year") +
   theme_bw()
-  
+
 ratings.fig
 
 #3
@@ -72,7 +62,6 @@ bench.fig
 filtered_data <- filtered_data %>% mutate(mkt_share = avg_enrollment/avg_eligibles)
 
 share.fig <- filtered_data %>% group_by(fips, year) %>%
-  select(fips, year, mkt_share) %>%
   ggplot(aes(x = year, y = mkt_share)) + 
   geom_line() +
   labs(title = "Share of Medicare Advanatge", x = "Year", y = "Market Share") +
@@ -80,24 +69,134 @@ share.fig <- filtered_data %>% group_by(fips, year) %>%
 share.fig
 
 #5
+data_09<- final.data2 %>% filter(year == 2009)
+data_09<- data_09 %>% mutate(raw_rating=rowMeans(
+  cbind(breastcancer_screen,rectalcancer_screen,cv_cholscreen,diabetes_cholscreen,
+        glaucoma_test,monitoring,flu_vaccine,pn_vaccine,physical_health,
+        mental_health,osteo_test,physical_monitor,primaryaccess,
+        hospital_followup,depression_followup,nodelays,carequickly,
+        overallrating_care,overallrating_plan,calltime,
+        doctor_communicate,customer_service,osteo_manage,
+        diabetes_eye,diabetes_kidney,diabetes_bloodsugar,
+        diabetes_chol,antidepressant,bloodpressure,ra_manage,
+        copd_test,betablocker,bladder,falling,appeals_timely,
+        appeals_review),
+  na.rm=T))
 
+final.data.2009 <- data_09[!is.na(data_09$Star_Rating), ]
+final.data.2009$indicator <- ifelse(final.data.2009$Star_Rating > final.data.2009$raw_rating, 1,0)
 
+plans_table <- final.data.2009 %>% group_by(Star_Rating) %>% summarize(avg_ind = mean(indicator))
 
+#6
+table_6 <- final.data.2009 %>%
+  mutate(score1 = raw_rating - 2.75,
+         score2 = raw_rating - 3.25, 
+         score3 = raw_rating - 3.75, 
+         score4 = raw_rating - 4.25,
+         mkt_share = avg_enrollment/avg_eligibles,
+         ln_share = log(mkt_share))
 
+reg_25 <- rdrobust(y=table_6$mkt_share, x=table_6$score1, c=0,
+                   h=0.125, p=1, kernel="uniform", vce="hc0",
+                   masspoints="off")
 
+reg_3 <- rdrobust(y=table_6$mkt_share, x=table_6$score2, c=0,
+                  h=0.125, p=1, kernel="uniform", vce="hc0",
+                  masspoints="off")
 
+reg_4 <- rdrobust(y=table_6$mkt_share, x=table_6$score3, c=0,
+                  h=0.125, p=1, kernel="uniform", vce="hc0",
+                  masspoints="off")
 
+reg_45 <-  rdrobust(y=table_6$mkt_share, x=table_6$score4, c=0,
+                    h=0.125, p=1, kernel="uniform", vce="hc0",
+                    masspoints="off")
 
+#7
+# bandwidth 0.1
+reg_25_2 <- rdrobust(y=table_6$mkt_share, x=table_6$score1, c=0,
+                   h=0.1, p=1, kernel="uniform", vce="hc0",
+                   masspoints="off")
 
+reg_3_2 <- rdrobust(y=table_6$mkt_share, x=table_6$score2, c=0,
+                  h=0.1, p=1, kernel="uniform", vce="hc0",
+                  masspoints="off")
 
+reg_4_2 <- rdrobust(y=table_6$mkt_share, x=table_6$score3, c=0,
+                  h=0.1, p=1, kernel="uniform", vce="hc0",
+                  masspoints="off")
 
+reg_45_2 <-  rdrobust(y=table_6$mkt_share, x=table_6$score4, c=0,
+                    h=0.1, p=1, kernel="uniform", vce="hc0",
+                    masspoints="off")
+#bandwidth 0.12
+reg_25_3 <- rdrobust(y=table_6$mkt_share, x=table_6$score1, c=0,
+                     h=0.12, p=1, kernel="uniform", vce="hc0",
+                     masspoints="off")
 
+reg_3_3 <- rdrobust(y=table_6$mkt_share, x=table_6$score2, c=0,
+                    h=0.12, p=1, kernel="uniform", vce="hc0",
+                    masspoints="off")
 
+reg_4_3 <- rdrobust(y=table_6$mkt_share, x=table_6$score3, c=0,
+                    h=0.12, p=1, kernel="uniform", vce="hc0",
+                    masspoints="off")
 
+reg_45_3 <-  rdrobust(y=table_6$mkt_share, x=table_6$score4, c=0,
+                      h=0.12, p=1, kernel="uniform", vce="hc0",
+                      masspoints="off")
+#bandwidth 0.13
+reg_25_4 <- rdrobust(y=table_6$mkt_share, x=table_6$score1, c=0,
+                     h=0.13, p=1, kernel="uniform", vce="hc0",
+                     masspoints="off")
 
+reg_3_4 <- rdrobust(y=table_6$mkt_share, x=table_6$score2, c=0,
+                    h=0.13, p=1, kernel="uniform", vce="hc0",
+                    masspoints="off")
 
+reg_4_4 <- rdrobust(y=table_6$mkt_share, x=table_6$score3, c=0,
+                    h=0.13, p=1, kernel="uniform", vce="hc0",
+                    masspoints="off")
 
+reg_45_4 <-  rdrobust(y=table_6$mkt_share, x=table_6$score4, c=0,
+                      h=0.13, p=1, kernel="uniform", vce="hc0",
+                      masspoints="off")
+#bandwidth 0.14
+reg_25_5 <- rdrobust(y=table_6$mkt_share, x=table_6$score1, c=0,
+                     h=0.14, p=1, kernel="uniform", vce="hc0",
+                     masspoints="off")
 
+reg_3_5 <- rdrobust(y=table_6$mkt_share, x=table_6$score2, c=0,
+                    h=0.14, p=1, kernel="uniform", vce="hc0",
+                    masspoints="off")
 
+reg_4_5 <- rdrobust(y=table_6$mkt_share, x=table_6$score3, c=0,
+                    h=0.14, p=1, kernel="uniform", vce="hc0",
+                    masspoints="off")
+
+reg_45_5 <-  rdrobust(y=table_6$mkt_share, x=table_6$score4, c=0,
+                      h=0.14, p=1, kernel="uniform", vce="hc0",
+                      masspoints="off")
+#bandwidth 0.15
+reg_25_6 <- rdrobust(y=table_6$mkt_share, x=table_6$score1, c=0,
+                     h=0.15, p=1, kernel="uniform", vce="hc0",
+                     masspoints="off")
+
+reg_3_6 <- rdrobust(y=table_6$mkt_share, x=table_6$score2, c=0,
+                    h=0.15, p=1, kernel="uniform", vce="hc0",
+                    masspoints="off")
+
+reg_4_6 <- rdrobust(y=table_6$mkt_share, x=table_6$score3, c=0,
+                    h=0.15, p=1, kernel="uniform", vce="hc0",
+                    masspoints="off")
+
+reg_45_6 <-  rdrobust(y=table_6$mkt_share, x=table_6$score4, c=0,
+                      h=0.15, p=1, kernel="uniform", vce="hc0",
+                      masspoints="off")
+
+fig_6<- rdplot(y=table_6$mkt_share, x=table_6$score, binselect="es",
+       title="RD Plot: Market Share", x.label="Summary Score",
+       y.label="Market Share", masspoints="off")
 
 
