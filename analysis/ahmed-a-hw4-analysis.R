e2e3aea722aad6bf994e8bd1aaa4ca0ca292edbe
Ammarah Ahmed <ammarah.rafia.ahmed@emory.edu>
2023-04-09 15:58:34 -0400

    attempt 3

--- analysis/ahmed-a-hw4-analysis.R
+++ analysis/ahmed-a-hw4-analysis.R
@@ -123,11 +123,10 @@ coef.45.1 <- tidy(star45, conf.int=TRUE) %>% mutate(rating=45)
 
 
 table.reg<- rbind(coef.30.1, coef.35.1, coef.40.1, coef.45.1) %>% 
-  mutate(bandwidth = 0.125)
+  dplyr::select(term, estimate, std.error, rating)
 table.reg$rating <- factor(table.reg$rating,
                            levels = c('30', '35', '40', '45'),
                            labels = c("3.0", "3.5", "4.0", "4.5"))
-table.reg <- table.reg %>% dplyr::select(term, estimate, std.error, rating, bandwidth)
 table.reg$term <- factor(table.reg$term,
                          levels = c('(Intercept)', 'treatTRUE', 'score'),
                          labels = c("(Intercept)", "Treatment", "Score"))
